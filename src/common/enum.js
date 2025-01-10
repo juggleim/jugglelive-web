ec5c313df0f87d22c606e239bcd9f6010d6d513c
luotm <shadowai5200@gmail.com>
2025-01-10 21:20:44 +0800

    feat: 资源地址

--- src/common/enum.js
+++ src/common/enum.js
@@ -17,8 +17,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1001',
     title: '植物大战僵尸首秀',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/1.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/1.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/1.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/1.mp4',
     count: '2,845',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/2fdf40b3-b625-4ede-b898-9ff4da939ce4.png',
@@ -28,8 +28,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1002',
     title: '敢不敢和我比比谁更甜',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/2.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/2.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/2.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/2.mp4',
     count: '6,821',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/210f1a21-e389-46e3-a6bd-97fcabf0736b.png',
@@ -39,8 +39,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1003',
     title: '今天和我一起拿下这个黑猴',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/3.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/3.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/3.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/3.mp4',
     count: '2,024',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/4d4fde6c-a74b-4c65-8cd6-adc9097d53a3.png',
@@ -50,8 +50,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1004',
     title: '爱滑雪，2024 滑雪季备赛',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/4.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/4.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/4.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/4.mp4',
     count: '3,948',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/d37a53f4-8593-4520-9500-182933644286.png',
@@ -61,8 +61,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1005',
     title: '战斗吧豌豆君，看我一决高下',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/5.png',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/5.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/5.png',
+    url: 'https://downloads.juggle.im/website/static/chatroom/5.mp4',
     count: '30,948',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/2f6c22f5-0043-4377-98f7-043c0b98532e.png',
@@ -72,8 +72,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1006',
     title: '倔强青铜的逆袭之路',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/6.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/6.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/6.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/6.mp4',
     count: '50,148',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/2d90da22-d42c-4250-866e-906e223293f3.png',
@@ -83,8 +83,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1007',
     title: '一个教唱歌的教授',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/7.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/7.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/7.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/7.mp4',
     count: '200,148',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/21421268-16bf-448e-9400-49e5f2964ed1.png',
@@ -94,8 +94,8 @@ export let CHATROOMS = [
   {
     id: 'chatroom1008',
     title: '咔咔咔卡个哇伊喽',
-    poster: 'https://downloads.juggleim.com/website/static/chatroom/8.jpeg',
-    url: 'https://downloads.juggleim.com/website/static/chatroom/8.mp4',
+    poster: 'https://downloads.juggle.im/website/static/chatroom/8.jpeg',
+    url: 'https://downloads.juggle.im/website/static/chatroom/8.mp4',
     count: '10,143',
     user: {
       avatar: 'https://file.lwoowl.cn/avatar_jgd/6499c545-4ce2-4d6f-bd72-b0e9435c9172.png',
